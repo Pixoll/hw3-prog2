17ded761542cd52304a08f7966e0c23fa21daef8
Crossfire7121 <146107730+Crossfire7121@users.noreply.github.com>
2023-11-01 23:40:47 -0300

    Imagenes de productos y monedas
    Agregadas las imagenes de los productos de la maquina y las monedas que se usaran.

--- src/gui/ImagenRecurso.java
+++ src/gui/ImagenRecurso.java
@@ -8,7 +8,16 @@ import java.util.Objects;
 public enum ImagenRecurso {
     ICONO("/images/icon.png"),
     FONDO("/images/background.png"),
-    MAQUINA("/images/machine.png");
+    MAQUINA("/images/machine.png"),
+    COCACOLA("/images/cocacola.png"),
+    SPRITE("/images/sprite.png"),
+    FANTA("/images/fanta.png"),
+    SNICKERS("/images/snickers.png"),
+    SUPER8("/images/super8.png"),
+    MONEDA100("/images/moneda100.png"),
+    MONEDA500("/images/moneda500.png"),
+    MONEDA1000("/images/moneda1000.png"),
+    MONEDA1500("/images/moneda1500.png");
 
     private final URL path;
     private final Image imagen;
